634e2c6663057306e5408798d5e5067758e1686b
Bernhard <b_walter@arcor.de>
2025-12-11 18:57:45 +0100

    support render_faces and render_edges

--- examples/box.py
+++ examples/box.py
@@ -1,13 +1,26 @@
 from build123d import *
+from tcv_screenshots import save_model, get_saved_models
+
+box = Box(1, 1, 1)
+box = fillet(box.edges(), 0.1)
+
+config = {
+    "cadWidth": 500,
+    "height": 375,
+    "render_edges": False,
+}
+
+save_model(box, "faces_only", config)
+
+config = {
+    "cadWidth": 500,
+    "height": 375,
+    "render_faces": False,
+}
+
+save_model(box, "edges_only", config)
 
 
 def main():
-    box = Box(1, 1, 1)
-    box = fillet(box.edges(), 0.1)
 
-    config = {
-        "cadWidth": 500,
-        "height": 375,
-    }
-
-    return box, config
+    return get_saved_models()
